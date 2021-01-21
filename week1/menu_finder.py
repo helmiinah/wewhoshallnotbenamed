de8cb2f6ebf20f44a849b4dbb918d3daf4eb5067
Helmiina <helmiina.hotti@hotmail.com>
2021-01-21 17:57:29 +0200

    Added extraction of menu items

--- week1/menu_finder.py
+++ week1/menu_finder.py
@@ -1,5 +1,8 @@
 from urllib import request
+from bs4 import BeautifulSoup
 from pprint import pprint  # For formatted printing/ pretty printing
+import re
+
 ######################################
 # Script to extract the day's menu   #
 # for Unicafe Kaivopiha              #
@@ -11,9 +14,16 @@ def main():
     # Download page as html
     url = 'https://www.lounaat.info/lounas/unicafe-ylioppilasaukio/helsinki'
     html = request.urlopen(url).read().decode('utf8')
-    # pprint(html)
+    # transform into beautifulsoup element:
+    soup = BeautifulSoup(html, 'html.parser')
 
-    # transform into beautifulsoup element
+    # Store all menu items (for every day available) to a list:
+    list_items_dishes = soup.findAll("li", {"class": re.compile(r"menu-item .*")})
+    for item in list_items_dishes:
+        pprint(item.get_text())
+
+    # next: extracting the days for which each menu item belongs to (or if we only want the menu for the current day?)
+    # and formatting
 
 
 main()
