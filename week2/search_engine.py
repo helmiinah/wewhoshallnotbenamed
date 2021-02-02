e13c21eb345f673d33513fb588e4da319162a929
Laura S <46318664+sinikala@users.noreply.github.com>
2021-02-02 21:31:06 +0200

    #9 Adds handling for most three word queries that include parenthesis and OOV words

--- week2/search_engine.py
+++ week2/search_engine.py
@@ -32,6 +32,54 @@ terms = cv.get_feature_names()
 # print(terms)
 
 
+def handle_oov_parenthesis(query):
+    # query = word AND/OR (word AND/OR word), ( word AND/OR word ) AND/OR word
+    # with an OOV somewhere and possible NOTs, except before parenthesis
+    query_parts = query.split()
+
+    if query.startswith('('):  # ( word AND/OR word ) AND/OR word
+        parenthesis = query[2:query.index(')')]
+        rest = query[query.index(')')+2:].strip()
+        rewritten_par = rewrite_query(parenthesis)
+        rewritten_rest = rewrite_query(re.sub('(and|or) ', '', rest))
+
+        if rest.startswith('and'):  # () AND <word>
+            if not rewritten_par or not rewritten_rest:  # either part is not valid
+                return None
+            else:
+                return '({}) & {}'.format(rewritten_par, rewritten_rest)
+
+        else:  # () OR <word>
+            rewritten_query = ''
+            if rewritten_par:
+                rewritten_query += '({}) | '.format(rewritten_par)
+            if rewritten_rest:
+                rewritten_query += rewritten_rest
+            return rewritten_query
+
+    else:  # word AND/OR (word AND/OR word)
+        start = query[:query.index('(')].strip()
+        parenthesis = query[query.index('(')+2:len(query)-2]
+        rewritten_start = rewrite_query(re.sub(' (and|or)', '', start))
+        rewritten_par = rewrite_query(parenthesis)
+
+        if start.endswith('and'):
+            if not rewritten_par or not rewritten_start:  # either part is not valid
+                return None
+            else:
+                return '{} & ({})'.format(rewritten_start, rewritten_par)
+        else:  # <word> OR ()
+            rewritten_query = ''
+            if rewritten_start:
+                rewritten_query = rewritten_start
+            if rewritten_par:
+                rewritten_query += ' | ({})'.format(rewritten_par)
+
+            return rewritten_query
+
+    return None
+
+
 def rewrite_token(t):
     return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t))
 
@@ -83,6 +131,10 @@ def rewrite_query(query):  # rewrite every token in the query
                         # query = "<NOT unknown> AND <known>"
                         # -> return matching documents for the last search term
                         return rewrite_query(query_parts[i+2])
+
+            elif '(' in query:
+                # bonus case: OOV query with parenthesis. Assumes query is grammatically correct
+                return handle_oov_parenthesis(query)
 
             elif i == len(query_parts) - 1:
                 # 3. case: token is last element of query
@@ -151,23 +203,24 @@ sparse_td_matrix = sparse_matrix.T.tocsr()
 
 while True:
     # test_query(query)
-    query = input("Add a query (press enter to quit): ").lower()
+    query = input("Add a query (press enter to quit): ").lower().strip()
     if query == "":
         break
-    print('Results:')
 
     try:
         if rewrite_query(query) is None:
             print("No matches.")
         else:
+            # print(rewrite_query(query))
             hits_matrix = eval(rewrite_query(query))
             hits_list = list(hits_matrix.nonzero()[1])
+            print('Results:')
             print("Matched", len(hits_list), "documents.")
 
             for doc_idx in hits_list[:10]:
                 print(
                     f"Matching doc: [{doc_idx}] {documents[doc_idx][:50]}...")
-    except (KeyError, SyntaxError):
+    except:  # (KeyError, SyntaxError):
         print('Bad query, could not perform a search.')
     print()
 # print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
