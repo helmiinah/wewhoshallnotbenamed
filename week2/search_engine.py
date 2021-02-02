3045e27c1f7d7f7731f218cf14006d71d472b563
Laura S <46318664+sinikala@users.noreply.github.com>
2021-02-02 19:56:51 +0200

    Refactors some variables, edits error handling to handle non-OOV related issues such as unbalanced parenthesis

--- week2/search_engine.py
+++ week2/search_engine.py
@@ -18,103 +18,93 @@ with open('text_data.txt', encoding="utf8") as file:
 
 soup = BeautifulSoup(contents, 'html.parser')
 
-test = soup.find_all('article')
-documents = [t.get_text().replace('\n', ' ') for t in test]
+articles = soup.find_all('article')
+documents = [t.get_text().replace('\n', ' ') for t in articles]
 # print('num of articles', len(articles))
 # print(cleaned_documents[0])
 
-
 cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r"(?u)\b\w+\b")
 sparse_matrix = cv.fit_transform(documents)
-
-# print("Term-document matrix: (?)\n")
-# print(sparse_matrix)
-
-# print("Term-document matrix: (?)\n")
 dense_matrix = sparse_matrix.todense()
-
-# print("Term-document matrix:\n")
 td_matrix = dense_matrix.T  # .T transposes the matrix
 
 terms = cv.get_feature_names()
-
-
 # print(terms)
 
 
 def rewrite_token(t):
-    # Can you figure out what happens here?
-
     return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t))
 
 
 def rewrite_query(query):  # rewrite every token in the query
-    for i in range(len(query.split())):
-        token = query.split()[i]
+    query_parts = query.split()
+    for i in range(len(query_parts)):
+        token = query_parts[i]
         if token not in terms and token not in d:
-
-            if len(query.split()) == 1:
+            # Token is an OOV word
+            if len(query_parts) == 1:
                 # 1. case: token is only element of query
                 # query = "unknown"
                 return None
 
             elif i == 0:
                 # 2. case: token is first element of query
-                next_token = query.split()[i + 1].lower()
+                next_token = query_parts[i + 1].lower()
                 if next_token == "and":
                     # query = "unknown AND ... "
                     return None
                 elif next_token == "or":
                     # query = "unknown OR ... "
                     # return documents matching word after OR
-                    return rewrite_token(query.split()[i + 2])
+                    return rewrite_token(query_parts[i + 2])
 
-            elif len(query.split()) == 2 and query.split()[i-1] == "not":
+            elif len(query_parts) == 2 and query_parts[i-1] == "not":
                 # query = "NOT unknown"
                 # return a 1x100 numpy array filled with ones -> matches all documents
                 return "np.ones((1,len(documents)), dtype=int)"
 
-            elif len(query.split()) == 4 and query.split()[i-1] == "not":
+            elif len(query_parts) == 4 and query_parts[i-1] == "not":
+                # queary: two words, AND/OR, and NOT
                 if i-2 > 0:
-                    if query.split()[i-2] == "or":
+                    if query_parts[i-2] == "or":
                         # query = "<known> OR NOT <unknown>"
                         # -> match all documents
                         return "np.ones((1,len(documents)), dtype=int)"
-                    if query.split()[i-2] == "and":
+                    if query_parts[i-2] == "and":
                         # query = "<known> AND NOT <unknown>"
                         # -> return matching documents for the first search term
-                        return rewrite_query(query.split()[i-3])
+                        return rewrite_query(query_parts[i-3])
                 else:
-                    if query.split()[i+1] == "or":
+                    if query_parts[i+1] == "or":
                         # query = "NOT <unknown> OR <known>"
                         # -> match all documents
                         return "np.ones((1,len(documents)), dtype=int)"
-                    if query.split()[i+1] == "and":
+                    if query_parts[i+1] == "and":
                         # query = "<NOT unknown> AND <known>"
                         # -> return matching documents for the last search term
-                        return rewrite_query(query.split()[i+2])
+                        return rewrite_query(query_parts[i+2])
 
-            elif i == len(query.split()) - 1:
+            elif i == len(query_parts) - 1:
                 # 3. case: token is last element of query
-                prev_token = query.split()[i - 1].lower()
+                prev_token = query_parts[i - 1].lower()
                 if prev_token == "and":
                     # query = " ... AND unknown"
                     return None
                 elif prev_token == "or":
                     # query = " ... OR unknown"
                     # return documents matching word before OR
-                    return rewrite_token(query.split()[i - 2])
-                #elif prev_token == "not":
+                    return rewrite_token(query_parts[i - 2])
+                # elif prev_token == "not":
                     # query = " ... NOT unknown"
                     # return documents matching word before NOT
                 #    return rewrite_token(query.split()[i - 2])
 
-            elif i != 0 and i < len(query.split()) - 1:
+            elif i != 0 and i < len(query_parts) - 1:
                 # 4. case: token is not first nor last element of query
-                prev_token = query.split()[i - 1].lower()
-                next_token = query.split()[i + 1].lower()
+                prev_token = query_parts[i - 1].lower()
+                next_token = query_parts[i + 1].lower()
                 # if there are two other words in the query in addition to unknown and both operator are "and":
-                if len(query.split()) == 5 and prev_token == "and" and next_token == "and":
+                if len(query_parts) == 5 and prev_token == "and" and next_token == "and":
                     return None
                 elif prev_token == "or" and next_token == "or":
                     # remove the current unknown word and the next operator "or" and recursively call function again:
@@ -141,7 +131,7 @@ def rewrite_query(query):  # rewrite every token in the query
                     # print(" ".join(new_query))  <- print to inspect new query
                     return rewrite_query(" ".join(new_query))
 
-    return " ".join(rewrite_token(t) for t in query.split())
+    return " ".join(rewrite_token(t) for t in query_parts)
 
 
 def test_query(query):
@@ -164,20 +154,24 @@ while True:
     query = input("Add a query (press enter to quit): ").lower()
     if query == "":
         break
-    print('Results')
+    print('Results:')
 
     try:
         if rewrite_query(query) is None:
-            print("Unknown word in query.")
+            print("No matches.")
         else:
             hits_matrix = eval(rewrite_query(query))
             hits_list = list(hits_matrix.nonzero()[1])
             print("Matched", len(hits_list), "documents.")
 
             for doc_idx in hits_list[:10]:
-                print(f"Matching doc: [{doc_idx}] {documents[doc_idx][:50]}...")
-    except KeyError:
-        print('Bad query')
+                print(
+                    f"Matching doc: [{doc_idx}] {documents[doc_idx][:50]}...")
+    except (KeyError, SyntaxError):
+        print('Bad query, could not perform a search.')
     print()
 # print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
 # print("The coordinates of the non-zero elements:", hits_matrix.nonzero())
+
+
+# refactoring, error handling
