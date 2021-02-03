57341a2a605f8ed3d73a6bfc4a6ed11d92fc3c31
Helmiina <helmiina.hotti@hotmail.com>
2021-02-03 15:10:18 +0200

    Cleaned code and generalized a rule about AND-searches and unknown words

--- week2/search_engine.py
+++ week2/search_engine.py
@@ -11,7 +11,7 @@ toy_documents = ["This is a silly example",
 d = {"and": "&", "AND": "&",
      "or": "|", "OR": "|",
      "not": "1 -", "NOT": "1 -",
-     "(": "(", ")": ")"}  # operator replacements
+     "(": "(", ")": ")"}
 
 with open('text_data.txt', encoding="utf8") as file:
     contents = file.read()
@@ -20,16 +20,12 @@ soup = BeautifulSoup(contents, 'html.parser')
 
 articles = soup.find_all('article')
 documents = [t.get_text().replace('\n', ' ') for t in articles]
-# print('num of articles', len(articles))
-# print(cleaned_documents[0])
 
 cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r"(?u)\b\w+\b")
 sparse_matrix = cv.fit_transform(documents)
 dense_matrix = sparse_matrix.todense()
 td_matrix = dense_matrix.T  # .T transposes the matrix
-
 terms = cv.get_feature_names()
-# print(terms)
 
 
 def handle_oov_parenthesis(query):
@@ -41,7 +37,7 @@ def handle_oov_parenthesis(query):
         parenthesis = query[2:query.index(')')]
         rest = query[query.index(')')+2:].strip()
         rewritten_par = rewrite_query(parenthesis)
-        rewritten_rest = rewrite_query(re.sub('(and|or) ', '', rest))
+        rewritten_rest = rewrite_query(re.sub(r'(and|or) ', '', rest))
 
         if rest.startswith('and'):  # () AND <word>
             if not rewritten_par or not rewritten_rest:  # either part is not valid
@@ -60,7 +56,7 @@ def handle_oov_parenthesis(query):
     else:  # word AND/OR (word AND/OR word)
         start = query[:query.index('(')].strip()
         parenthesis = query[query.index('(')+2:len(query)-2]
-        rewritten_start = rewrite_query(re.sub(' (and|or)', '', start))
+        rewritten_start = rewrite_query(re.sub(r' (and|or)', '', start))
         rewritten_par = rewrite_query(parenthesis)
 
         if start.endswith('and'):
@@ -77,8 +73,6 @@ def handle_oov_parenthesis(query):
 
             return rewritten_query
 
-    return None
-
 
 def rewrite_token(t):
     return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t))
@@ -86,6 +80,7 @@ def rewrite_token(t):
 
 def rewrite_query(query):  # rewrite every token in the query
     query_parts = query.split()
+    operators = [x for x in query_parts if x in d and x not in "()"]
     for i in range(len(query_parts)):
         token = query_parts[i]
         if token not in terms and token not in d:
@@ -94,7 +89,9 @@ def rewrite_query(query):  # rewrite every token in the query
                 # 1. case: token is only element of query
                 # query = "unknown"
                 return None
-
+            elif all(o.lower() == "and" for o in operators):
+                # All operators are "and" and there's an OOV-word and no parentheses = no matches
+                return None
             elif i == 0:
                 # 2. case: token is first element of query
                 next_token = query_parts[i + 1].lower()
@@ -112,7 +109,7 @@ def rewrite_query(query):  # rewrite every token in the query
                 return "np.ones((1,len(documents)), dtype=int)"
 
             elif len(query_parts) == 4 and query_parts[i-1] == "not":
-                # queary: two words, AND/OR, and NOT
+                # query: two words, AND/OR, and NOT
                 if i-2 > 0:
                     if query_parts[i-2] == "or":
                         # query = "<known> OR NOT <unknown>"
@@ -157,10 +154,7 @@ def rewrite_query(query):  # rewrite every token in the query
                 # 4. case: token is not first nor last element of query
                 prev_token = query_parts[i - 1].lower()
                 next_token = query_parts[i + 1].lower()
-                # if there are two other words in the query in addition to unknown and both operator are "and":
-                if len(query_parts) == 5 and prev_token == "and" and next_token == "and":
-                    return None
-                elif prev_token == "or" and next_token == "or":
+                if prev_token == "or" and next_token == "or":
                     # remove the current unknown word and the next operator "or" and recursively call function again:
                     new_query = query.split()
                     new_query.pop(i)
@@ -191,18 +185,14 @@ def rewrite_query(query):  # rewrite every token in the query
 def test_query(query):
     print("Query: '" + query + "'")
     print("Rewritten:", rewrite_query(query))
-    # Eval runs the string as a Python command
     print("Matching:", eval(rewrite_query(query)))
     print()
 
 
-t2i = cv.vocabulary_  # shorter notation: t2i = term-to-index
-# print("Query: example")
-# print(td_matrix[t2i["example"]])
-
+t2i = cv.vocabulary_
 sparse_td_matrix = sparse_matrix.T.tocsr()
-# print(sparse_td_matrix)
 
+print("Welcome to English Wikipedia search engine!")
 while True:
     query = input("Add a query (press enter to quit): ").lower().strip()
     # test_query(query)
@@ -213,7 +203,6 @@ while True:
         if rewrite_query(query) is None:
             print("No matches.")
         else:
-            # print(rewrite_query(query))
             hits_matrix = eval(rewrite_query(query))
             hits_list = list(hits_matrix.nonzero()[1])
             print('Results:')
@@ -225,8 +214,5 @@ while True:
     except:  # (KeyError, SyntaxError):
         print('Bad query, could not perform a search.')
     print()
-# print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
-# print("The coordinates of the non-zero elements:", hits_matrix.nonzero())
-
 
 # refactoring, error handling
