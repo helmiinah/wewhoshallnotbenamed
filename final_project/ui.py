52eebe9a1463cd3f692b75a0fc78f51f322c2392
Helmiina <helmiina.hotti@hotmail.com>
2021-03-05 16:44:34 +0200

    Added a link to the Wikipedia page of the wine variety if it exists #24 #36

--- final_project/ui.py
+++ final_project/ui.py
@@ -11,6 +11,7 @@ import re
 import math
 import pandas as pd
 from random import randint
+import requests
 
 # Initialize Flask instance
 app = Flask(__name__)
@@ -96,6 +97,67 @@ def search():
     return render_template('index.html', matches=matches, number=len(matches), query=query, engine_choice=engine_choice, plot_path=plot_path)
 
 
+@app.route('/search/<id>')
+def show_document(id):
+    query = request.args.get('query')
+    query = query.lower()
+
+    engine_choice = request.args.get('engine')
+
+    idx = int(id)
+    wine = engine.reviews.iloc[idx]
+    country_codes = engine.country_codes
+
+    # Count word matches inside document
+    doc_matches = 0
+    for word in wine["description"].split() + wine["variety"].split():
+        if engine_choice == "boolean":
+            query_splitted = query.split()
+
+            if "and" in query_splitted:
+                query_splitted.remove("and")
+            if "or" in query_splitted:
+                query_splitted.remove("or")
+            if "not" in query_splitted:
+                query_splitted.remove("not")
+
+            if word.lower().strip(",.;:!?") in query_splitted:
+                print(word.lower().strip(",.;:!?"))
+                doc_matches += 1
+
+        elif engine_choice == "relevance":
+            # Check if query was exact match search
+            if '"' in query:
+                if query.strip('""') == word.lower().strip(",.;:!?"):
+                    doc_matches += 1
+            elif '*' in query:
+                if query.strip('*') in word.lower().strip(",.;:!?"):
+                    doc_matches += 1
+            else:
+                for q_word in query.split():
+                    if q_word == word.lower().strip(",.;:!?") or q_word in word.lower().strip(",.;:!?"):
+                        doc_matches += 1
+
+    generate_plot(idx, wine["description"], wine["title"])
+    generate_wordcloud(idx, wine["description"])
+
+    wiki_path = re.sub(r"\s", r"_", wine["variety"])
+    print(wiki_path)
+
+    try:
+        response = requests.get(f"https://en.wikipedia.org/wiki/{wiki_path}")
+        print(response.status_code)
+        if str(response.status_code).startswith('2'):
+            return render_template('wine.html', idx=str(idx), query=query, flag=country_codes[wine['country']],
+                                   num_matches=doc_matches, engine=engine_choice, wine=wine, wiki_path=wiki_path)
+        else:
+            return render_template('wine.html', idx=str(idx), query=query, flag=country_codes[wine['country']],
+                                num_matches=doc_matches, engine=engine_choice, wine=wine)
+    except:
+        return render_template('wine.html', idx=str(idx), query=query, flag=country_codes[wine['country']],
+                                num_matches=doc_matches, engine=engine_choice, wine=wine)
+
+
 def generate_plot(idx, document, name):
     extractor = pke.unsupervised.TopicRank()
     extractor.load_document(input=document, language='en')
@@ -160,52 +222,6 @@ def generate_country_plot(matches, plot_path):
     plt.close()
 
 
-@app.route('/search/<id>')
-def show_document(id):
-    query = request.args.get('query')
-    query = query.lower()
-
-    engine_choice = request.args.get('engine')
-
-    idx = int(id)
-    wine = engine.reviews.iloc[idx]
-    country_codes = engine.country_codes
-
-    # Count word matches inside document
-    doc_matches = 0
-    for word in wine["description"].split() + wine["variety"].split():
-        if engine_choice == "boolean":
-            query_splitted = query.split()
-
-            if "and" in query_splitted:
-                query_splitted.remove("and")
-            if "or" in query_splitted:
-                query_splitted.remove("or")
-            if "not" in query_splitted:
-                query_splitted.remove("not")
-
-            if word.lower().strip(",.;:!?") in query_splitted:
-                print(word.lower().strip(",.;:!?"))
-                doc_matches += 1
-
-        elif engine_choice == "relevance":
-            # Check if query was exact match search
-            if '"' in query:
-                if query.strip('""') == word.lower().strip(",.;:!?"):
-                    doc_matches += 1
-            elif '*' in query:
-                if query.strip('*') in word.lower().strip(",.;:!?"):
-                    doc_matches += 1
-            else:
-                for q_word in query.split():
-                    if q_word == word.lower().strip(",.;:!?") or q_word in word.lower().strip(",.;:!?"):
-                        doc_matches += 1
-
-    generate_plot(idx, wine["description"], wine["title"])
-    generate_wordcloud(idx, wine["description"])
-
-    return render_template('wine.html', idx=str(idx), query=query, flag=country_codes[wine['country']],
-                           num_matches=doc_matches, engine=engine_choice, wine=wine)
 
 
 # @app.route('/search/')
